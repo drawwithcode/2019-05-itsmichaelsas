7d16a8a4c0002a81068e7f94022af8dcc66b5d4c
itsmichaelsas <55444847+itsmichaelsas@users.noreply.github.com>
2019-10-22 09:04:25 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,78 @@
+var mic;
+var take;
+var voice;
+
 function preload(){
-  // put preload code here
+
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth,windowHeight);
+  background(0);
+
+  mic = new p5.AudioIn();
+  mic.start();
+
+  var lang = navigator.language || 'en-US';
+  voice = new p5.SpeechRec(lang, showResult);
+  voice.continuous = true;
+  voice.interimResults = true;
+  voice.onResult = showResult;
+  voice.start();
+
+  take = createCapture(VIDEO);
+  take.size(windowWidth, windowHeight);
+  take.hide();
+  pixelDensity(0.4);
 }
 
 function draw() {
-  // put drawing code here
+  var vol = mic.getLevel();
+
+  var myCatch = take.loadPixels();
+  image(myCatch, 0, 0, windowWidth, windowHeight);
+  filter(GRAY);
+
+  push();
+  var talkText = 'take a moment, talk to yourself';
+  fill('white');
+  textFont('Jomolhari');
+  textAlign(CENTER, TOP);
+  textSize(40);
+  text(talkText,width/2, 60);
+  pop();
+
+  push();
+  var underText = 'press enter to screenshot';
+  fill('white');
+  textFont('Jomolhari');
+  textAlign(CENTER, TOP);
+  textSize(30);
+  text(underText,width/2, 100);
+  pop();
+
+  push();
+  fill('yellow');
+  textFont('Jomolhari');
+  textAlign(CENTER, CENTER);
+  textStyle(BOLDITALIC);
+  textSize(150);
+  text(voice.resultString, width/2, height/2);
+  pop();
+}
+
+function showResult() {
+  if (voice.resultValue){
+  createP(voice.resultString);
+}
+}
+
+function keyPressed() {
+  if (keyCode === ENTER) {
+  saveCanvas('slitScan', 'png');
+}
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
